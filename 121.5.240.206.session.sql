b61b6a5b6c63dd303ff88885c2b9ac22c7a768a1
linjinbao66 <linjinbao66@gmail.com>
2021-12-28 00:05:36 +0800

    update

---
+++ 121.5.240.206.session.sql
@@ -0,0 +1,2 @@
+USE vrmanager;
+CREATE TABLE clazz_user(id BIGINT(20), clazz_no BIGINT(20), user_sn BIGINT(20));
--- update.sql
+++ update.sql
@@ -5,3 +5,8 @@ update user set password='toor' where username='admin' and sn='admin';
 
 -- clazz表增加clazz_no列
 ALTER TABLE clazz ADD COLUMN clazz_no BIGINT(20) NULL;
+
+-- 增加表clazz_user
+
+USE vrmanager;
+CREATE TABLE clazz_user(id BIGINT(20), clazz_no BIGINT(20), user_sn BIGINT(20));
